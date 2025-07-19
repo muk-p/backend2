771cd06e03035bfc6408b47cb90acbaff82af374
Mukara <jmukara959@gmail.com>
2025-07-19 03:37:42 +0300

    Update 20231027120000

--- migrations/20231027120000-add-role-to-users.js
+++ migrations/20231027120000-add-role-to-users.js
@@ -14,3 +14,15 @@ module.exports = {
   }
 };
 
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Products', 'imageFile', {
+      type: Sequelize.BLOB('long'),
+      allowNull: true,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Products', 'imageFile');
+  }
+};
